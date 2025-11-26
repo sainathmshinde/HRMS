c11f149edeeabb94e9fb22196b3a35f26de16175
RushiTaware4664 <Rushikesh.taware@syborgtech.com>
2025-11-26 20:13:28 +0530

    Leave Reports and payroll

--- src/assets/js/common.js
+++ src/assets/js/common.js
@@ -1,36 +1,51 @@
 document.addEventListener("DOMContentLoaded", function () {
-
   const sidebar = document.getElementById("sidebar");
   if (!sidebar) return;
   sidebar.querySelectorAll('.nav > .nav-item > a.nav-link[data-bs-toggle="collapse"]').forEach(link => {
     link.removeAttribute("data-bs-toggle");
-
     link.addEventListener("click", function (e) {
       e.preventDefault();
-
       const targetSelector = this.getAttribute("href");
       if (!targetSelector || !targetSelector.startsWith("#")) return;
-
       const target = document.querySelector(targetSelector);
       if (!target) return;
-
       let bsCollapse = bootstrap.Collapse.getInstance(target);
       if (!bsCollapse) {
         bsCollapse = new bootstrap.Collapse(target, { toggle: false });
       }
-
       bsCollapse.toggle();
     });
   });
+  const exportBtn = document.getElementById("btnExportExcel");
+  if (exportBtn) {
+    exportBtn.addEventListener("click", () => {
+      const content = "Employee Name,Leave Date\nRahul,12-Oct-2025";
+
+      const blob = new Blob([content], {
+        type: "application/vnd.ms-excel"
+      });
+
+      const url = URL.createObjectURL(blob);
+
+      const a = document.createElement("a");
+      a.href = url;
+      a.download = "OnLeaveReport.xlsx"; 
+
+      document.body.appendChild(a);
+      a.click();
+      a.remove();
+
+      URL.revokeObjectURL(url);
+    });
+  }
+});
   sidebar.addEventListener("click", function (e) {
     const link = e.target.closest("a.nav-link");
     if (!link) return;
     sidebar.querySelectorAll(".nav-item.active").forEach(i => i.classList.remove("active"));
     sidebar.querySelectorAll(".sub-menu .nav-link.active").forEach(a => a.classList.remove("active"));
-
     const navItem = link.closest(".nav-item");
     const isSubMenu = link.closest(".sub-menu");
-
     if (isSubMenu) {
       link.classList.add("active");
       const parent = link.closest(".collapse")?.closest(".nav-item");
@@ -39,5 +54,3 @@ document.addEventListener("DOMContentLoaded", function () {
       navItem.classList.add("active");
     }
   });
-
-});
